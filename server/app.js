77ce7b5331978f8bfb5b2e4ae23e507d4a1de781
David Cady Jr <davidcadyjr@mac.com>
2017-06-22 17:25:26 -0500

    patch working

--- server/app.js
+++ server/app.js
@@ -22,6 +22,21 @@ app.post('/theNorth', function(request, response){
 	response.json("success");
 })
 
+app.patch('/theNorth/:index', function(request, response){
+	
+	var house = request.body.house;
+	
+	var index = request.params.index;
+	theNorth[index] = house;
+	
+	response.json("success");
+})
+
+app.delete('/theNorth', function(request, response){
+
+
+})
+
 
 server.listen(3000, function(){
 	console.log("listen to port 3000 your Mom");
