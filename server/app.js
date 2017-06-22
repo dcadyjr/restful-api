6f6bbf6efd4d23ce5e743b13809fde62d5cda506
David Cady Jr <davidcadyjr@mac.com>
2017-06-22 17:46:39 -0500

    patch and delete work. homework finished

--- server/app.js
+++ server/app.js
@@ -25,18 +25,23 @@ app.post('/theNorth', function(request, response){
 app.patch('/theNorth/:index', function(request, response){
 	
 	var house = request.body.house;
-	
 	var index = request.params.index;
 	theNorth[index] = house;
-	
 	response.json("success");
 })
 
-app.delete('/theNorth', function(request, response){
+app.delete('/theNorth/:index', function(request, response){
 
+	var house = request.body.house;
+	console.log(house);
+	console.log(theNorth);
+	var index = request.params.index;
+	theNorth.splice(index);
+	console.log(theNorth);
+
+	response.json('success');
 
 })
-
 
 server.listen(3000, function(){
 	console.log("listen to port 3000 your Mom");
